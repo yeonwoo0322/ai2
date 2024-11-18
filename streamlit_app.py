6509a08fc2acc8670d5bf632a1da70c71c56590d
yeonwoo0322 <20242512@sdhs.gwe.hs.kr>
2024-11-18 15:11:35 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,11 +52,11 @@ if uploaded_file is not None:
     st.markdown("<h3>클래스별 확률:</h3>", unsafe_allow_html=True)
 
     if prediction == labels[0]:
-         st.write("중냉 꿋굿")
+         st.write("중냉 비쥬얼 역겹긔")
     elif prediction == labels[1]:
          st.write("짜장면 개 쌈@뽕하긔")
     elif prediction == labels[2]:
-         st.write("짬뽕은 맵지만 맛있어!!")
+         st.write("짬뽕은 야무지긔!!")
 
     for label, prob in zip(labels, probs):
         # HTML 및 CSS로 확률을 시각화
